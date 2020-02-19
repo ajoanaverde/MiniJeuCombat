c3fab5a2bf3bb8c7a74d8da2fc750fbb9dadd735
Melanie <gimmelli.melanie@gmail.com>
2020-02-19 11:31:00 +0100

    mise en place des setter et du contructeur de la classe Mage

--- classes/Mage.class.php
+++ classes/Mage.class.php
@@ -5,7 +5,16 @@ class Mage extends Personnage
    private $sortilège;
    private $mana;
 
+   //constructeur 
 
+   public function __construct($nom, $force, $sante, $sortilège, $mana)
+   {
+      $this->setNom($nom);
+      $this->setForce($force);
+      $this->setSante($sante);
+      $this->setSortilège($sortilège);
+      $this->setMana($mana);
+   }
 
 
    //getters
@@ -26,5 +35,26 @@ class Mage extends Personnage
    public function getMana()
    {
       return $this->mana;
+   }
+
+   //setters
+
+   /**
+    * set value $sortilège
+    * type : string
+    */
+   public function setSortilège(string $sortilège)
+   {
+      $this->sortilège = $sortilège;
+   }
+
+
+   /**
+    * set value $mana
+    * type : string
+    */
+   public function setMana(int $mana)
+   {
+      $this->mana = $mana;
    }
 }
