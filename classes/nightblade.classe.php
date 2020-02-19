7924dc12e732bcd94dbb00e9841dbb58146701e0
ajoanaverde <ajoanaverde@gmail.com>
2020-02-19 20:56:46 +0100

    .

--- classes/nightblade.classe.php
+++ classes/nightblade.classe.php
@@ -7,15 +7,20 @@ private $assassinat;
 private $ombre;
 private $siphoner;
 
-    public function __construct($nom, $force, $sante, $mana, $endurance)
+    public function __construct($nom, $force, $sante, $mana, $endurance, $assassinat, $ombre, $siphoner)
     {
         parent::__construct(["nom" => $nom, "force" => $force, "sante" => $sante, "mana" => $mana, "endurance" => $endurance]);
+        $this->assassinat = $assassinat;
+        $this->ombre = $ombre;
+        $this->siphoner = $siphoner;
     }
 
 
 }
 
 //$janette = new Nightblade(["nom" => "Janette", "force" => 30, "sante" => 70, "mana" => 20, "endurance" => 90]);
-$janette = new Nightblade("Janette", 10, 60, 10, 100);
+//$janette = new Nightblade("Janette", 10, 60    public function getValue(){
+        
+//}, 10, 100, 1, 1, 1);
 $janette->parler($janette);
 $janette->lireStats($janette);
