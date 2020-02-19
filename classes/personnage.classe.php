f69a35953d85beb40dd193ec810b41869a0f0c81
ajoanaverde <ajoanaverde@gmail.com>
2020-02-19 09:54:03 +0100

    personnage getters et setters

--- classes/personnage.classe.php
+++ classes/personnage.classe.php
@@ -1,5 +1,62 @@
 <?php
 
-class Personnage{
-    
+class Personnage
+{
+    private $nom;
+    private $force;
+    private $sante;
+
+    // CONSTRUCTOR
+
+    // GETTERS
+
+    //  return value Nom
+    //  type string
+
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    //  return value Force
+    //  type integer
+
+    public function getForce()
+    {
+        return $this->force;
+    }
+
+    //  return value Sante
+    //  type integer
+
+    public function getSante()
+    {
+        return $this->sante;
+    }
+
+    //   SETTERS
+
+    //  set value Nom
+    //  type string
+
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    //  set value Force
+    //  type integer
+
+    public function setForce($force)
+    {
+        $this->force = $force;
+    }
+
+    //  set value Sante
+    //  type integer
+
+    public function setSante($sante)
+    {
+        $this->sante = $sante;
+    }
 }
